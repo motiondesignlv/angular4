4eaa9b28716fb57424029daac245d00ce3cd3fa2
Luis <dynalabcreative@gmail.com>
2017-10-21 00:59:38 -0400

    wip component detalles

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -10,19 +10,20 @@ import { ActivatedRoute } from '@angular/router';
 export class DetalleComponent {
     lugares:any = [
         
-          {id: 1, plan: 'pagado',   cercania:1, distancia: 1,   active: true, nombre:'floristeria numero 1'},
-          {id: 2, plan: 'gratuito', cercania:1, distancia: 1.8, active: false, nombre:'floristeria numero 2'},
-          {id: 3, plan: 'pagado',   cercania:2, distancia: 2.5, active: true, nombre:'floristeria numero 3'},
-          {id: 4, plan: 'pagado',   cercania:3, distancia: 6.4, active: true, nombre:'floristeria numero 4'},
-          {id: 5, plan: 'gratuito', cercania:3, distancia: 4.8, active: false, nombre:'floristeria numero 5'},
-          {id: 6, plan: 'pagado',   cercania:2, distancia: 2.8, active: true, nombre:'floristeria numero 6'},
-          {id: 7, plan: 'gratuito', cercania:3, distancia: 7,   active: true, nombre:'floristeria numero 7'},
-          {id: 8, plan: 'pagado',   cercania:1, distancia: 0.8, active: true, nombre:'floristeria numero 8'},
-          {id: 9, plan: 'pagado',   cercania:2, distancia: 2.3, active: false, nombre:'floristeria numero 9'}
+          {id: 1, plan: 'pagado',   cercania:1, distancia: 1,   active: true, nombre:'floristeria numero 1', description: 'descripcion de este negocio, mas adelante tendremos mas informacion'},
+          {id: 2, plan: 'gratuito', cercania:1, distancia: 1.8, active: false, nombre:'floristeria numero 2', description: 'descripcion de este negocio, mas adelante tendremos mas informacion'},
+          {id: 3, plan: 'pagado',   cercania:2, distancia: 2.5, active: true, nombre:'floristeria numero 3', description: 'descripcion de este negocio, mas adelante tendremos mas informacion'},
+          {id: 4, plan: 'pagado',   cercania:3, distancia: 6.4, active: true, nombre:'floristeria numero 4', description: 'descripcion de este negocio, mas adelante tendremos mas informacion'},
+          {id: 5, plan: 'gratuito', cercania:3, distancia: 4.8, active: false, nombre:'floristeria numero 5', description: 'descripcion de este negocio, mas adelante tendremos mas informacion'},
+          {id: 6, plan: 'pagado',   cercania:2, distancia: 2.8, active: true, nombre:'floristeria numero 6', description: 'descripcion de este negocio, mas adelante tendremos mas informacion'},
+          {id: 7, plan: 'gratuito', cercania:3, distancia: 7,   active: true, nombre:'floristeria numero 7', description: 'descripcion de este negocio, mas adelante tendremos mas informacion'},
+          {id: 8, plan: 'pagado',   cercania:'8', distancia: '0.8', active: true, nombre:'floristeria numero 8', description: 'descripcion de este negocio, mas adelante tendremos mas informacion'},
+          {id: 9, plan: 'pagado',   cercania:2, distancia: 2.3, active: false, nombre:'floristeria numero 9', description: 'descripcion de este negocio, mas adelante tendremos mas informacion'}
        
-      ];
+      ]
 
       id= null;
+      lugar:any = {};
 
 
  constructor(private route: ActivatedRoute){
@@ -30,9 +31,10 @@ export class DetalleComponent {
       console.log(this.route.snapshot.queryParams['action']);
       this.id = this.route.snapshot.params['id'];
       console.log(this.buscarLugar())
+      this.lugar = this.buscarLugar();
  }  
 
  buscarLugar(){
-     return this.lugares.filter( (lugar) => { return lugar.id = this.id })[0] || null; 
+     return this.lugares.filter( (lugar) => { return lugar.id == this.id } )[0] || null; 
  }
 }
